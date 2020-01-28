2c66b2f08bd12b51fce45343996cdd443f32068b
Abner Herrera-Chinea <abner.herrera-chinea@g.austincc.edu>
2020-01-28 14:05:28 -0600

    I have completed Homework 1 and tested it using a unit test.

--- src/homework/01_expressions/main.cpp
+++ src/homework/01_expressions/main.cpp
@@ -1,15 +1,21 @@
 ﻿//write include statements
+# include "expressions.h"
+# include <iostream>
 
 
 //write namespace using statement for cout
-
+using std::cout;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	int num1 = 10;
+	int num2 = 10;
+
+	cout << multiply_numbers(num1, num2) << "\n";
 
 	return 0;
+
 }
